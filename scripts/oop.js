20441a53af59aec87ab80f773ab15217fd3e050d
lewostrowski <43729519+lewostrowski@users.noreply.github.com>
2019-01-10 20:07:29 +0100

    Add files via upload
    v1.2 -stable
    / added logs
    / added function when unable to get JSON file (error message, clock)
    / added few comment

--- scripts/oop.js
+++ scripts/oop.js
@@ -28,9 +28,9 @@ const dayRemain = function(x, y) {
   } else if (minutes <= 59 && minutes > 0) {
     return minutes + " m"
   } else if (seconds <= 0) {
-    return "Night"
+    return 1
   } else {
-    return "Error"
+    return 0
   }
 }
 
@@ -48,21 +48,22 @@ const dayDuration = function(x) {
       }
     return minutes
   } else if (x < 0) {
-    return "Night"
+    return 1
   } else {
-    return "Error"
+    return 0
   }
 }
 
 let req = new XMLHttpRequest()
-// const jsonLink = "scripts/json.json"
-const jsonLink = "https://api.sunrise-sunset.org/json?lat=52.406341&lng=16.932882&formatted=0"
+ const jsonLink = "scripts/j son.json" // for test puropse only
+//const jsonLink = "https://api.sunrise-sunset.org/json?lat=52.406341&lng=16.932882&formatted=0"
 
 const sunInfo = () => {
   req.addEventListener("readystatechange", (e) => {
     if (req.readyState === 4 && req.status === 200) {
       let data = JSON.parse(e.target.responseText)
-      console.log("Data taken and parsed") // controla message
+      console.log("JSON file status: OK")
+      console.log("JSON parsing: OK") // control message
       data = data.results
 
       const nonStaticData = () => {
@@ -77,7 +78,7 @@ const sunInfo = () => {
         let dayRem = null
 
         // check whether display sunlight time remain or information about night
-        if (dayRemain(moment(data.sunset).unix(), moment().unix()) !== "Night"){
+        if (dayRemain(moment(data.sunset).unix(), moment().unix()) !== 1){
           dayRem = new ElementNew("day-remain", dayRemain(moment(data.sunset).unix(), moment().unix()), "sun-remain_")
         } else {
           dayRem = new ElementNew("day-remain", "is-night_1")
@@ -129,17 +130,41 @@ const sunInfo = () => {
       const inter2 = setInterval(clockDisplay, 1000)
 
     } else if (req.readyState === 4) {
-      let newDiv = document.createElement("div")
-      let errorMsg = document.createElement("p")
-      errorMsg.setAttribute("id", "civil-set")
-      errorMsg.textContent = "Error: " + e.target.status
-      newDiv.appendChild(errorMsg)
-      document.querySelector(".info-container").appendChild(newDiv)
+      let i = 0
+      let newDiv4 = document.createElement("div")
+      newDiv4.setAttribute("class", "error-msg")
+      for (i=0;i<9;i++) {
+        let errorMsg = new ElementNew("error-msq", e.target.status, "error_")
+        newDiv4.appendChild(errorMsg.make())
+      }
+      document.querySelector(".info-container").appendChild(newDiv4)
+
+      const clockDisplay = () => {
+        document.querySelector(".clock-container").innerHTML = ""
+        let clock = new ElementNew("clock", moment().format("HH:mm:ss"))
+        let date = new ElementNew("date", moment().format("DD/MM/YYYY"))
+
+        let newDiv2 = document.createElement("div")
+        newDiv2.setAttribute("class", "time-stamp")
+        newDiv2.appendChild(clock.make())
+        newDiv2.appendChild(date.make())
+        document.querySelector(".clock-container").appendChild(newDiv2)
+
+        let newDiv3 = document.createElement("div")
+        newDiv3.setAttribute("class", "show-info")
+        let showInfo = new ElementNew("showInfo", "Better check internet connection")
+        console.log("If internet connection is ok, check JSON file.")
+        newDiv3.appendChild(showInfo.make())
+        document.querySelector(".clock-container").appendChild(newDiv3)
+      }
+      clockDisplay()
+      const inter2 = setInterval(clockDisplay, 1000)
     }
 
   })
   req.open("GET", jsonLink)
   req.send()
+  console.log("JSON note controler. Action taken once at " + moment().format("HH:mm:ss"))
 }
 
 sunInfo()
